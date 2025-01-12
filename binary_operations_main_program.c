768904bcf71a97fc09bfd14791ff5f0ec64ceb3a
MauroPresso <mauro.presso@gmail.com>
2025-01-12 18:53:42 -0300

    Ahora imprime bien est├®tico los resultados.

--- binary_operations_main_program.c
+++ binary_operations_main_program.c
@@ -8,33 +8,34 @@
 int main()
 {
     // SET_BIT
-    
+    printf("\n");
     testing_set_bit32();
-
+    printf("\n");
     testing_set_bit64();
-
-    
+    printf("\n");
+    printf("\n");
     // TOGGLE_BIT
-
+    printf("\n");
     testing_toggle_bit32();
-
+    printf("\n");
     testing_toggle_bit64();
-
-
+    printf("\n");
+    printf("\n");
     // CARRY_ROTATE
-
+    printf("\n");
     testing_carry_rotate32();
-
+    printf("\n");
     testing_carry_rotate64();
-
-
+    printf("\n");
+    printf("\n");
     // EXTRACT_BITS_SEGMENT
-
+    printf("\n");
     testing_extract_bits_segment32();
-
+    printf("\n");
     testing_extract_bits_segment64();
-
-
+    printf("\n");
+    printf("\n");
     // FIN DEL PROGRAMA
+    printf("FIN DEL PROGRAMA");
     return 0;
 }
