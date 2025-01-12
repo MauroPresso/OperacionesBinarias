d213a117bd19b051c17331588591c89800d50ab7
MauroPresso <mauro.presso@gmail.com>
2025-01-12 16:03:26 -0300

    This is the full filled, and documented too, version of binary_operations_test_voids_lib.h

---
+++ binary_operations_test_voids_lib.h
@@ -0,0 +1,45 @@
+/**
+ * @file binary_operations_test_voids_library.h
+ * @brief Declaraciones de procedimientos para testear funciones de manipulación de datos a nivel de bits (de 32 y 64 bits).
+ *
+ * Este archivo contiene las declaraciones de funciones para manipular datos 
+ * a nivel de bits (de 32 y 64 bits).
+ */
+
+#pragma once
+#include <stdint.h>
+#include "binary_operations_functions_library.h"
+
+// SHOW_BIN
+
+void testing_show_bin32(void);
+
+void testing_show_bin64(void);
+
+
+// SET_BIT
+
+void testing_set_bit32(void);
+
+void testing_set_bit64(void);
+
+
+// TOGGLE_BIT
+
+void testing_toggle_bit32(void);
+
+void testing_toggle_bit64(void);
+
+
+// CARRY_ROTATE
+
+void testing_carry_rotate32(void);
+
+void testing_carry_rotate64(void);
+
+
+// EXTRACT_BITS_SEGMENT
+
+void testing_extract_bits_segment32(void);
+
+void testing_extract_bits_segment64(void);
